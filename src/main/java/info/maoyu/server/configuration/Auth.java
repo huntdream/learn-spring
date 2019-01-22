3d059ee217db0eeff40702072014c9e90b7583d4
Yu Mao <maoyu960320@hotmail.com>
2019-01-22 21:36:58 +0800

    Auth init(not completely configured)

---
+++ src/main/java/info/maoyu/server/configuration/Auth.java
@@ -0,0 +1,16 @@
+package info.maoyu.server.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class Auth extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/", "/user").permitAll();
+    }
+}
