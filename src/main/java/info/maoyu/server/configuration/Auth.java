d01b17276395ee3a8abcf6dcb16872ab14a64fc1
Yu Mao <maoyu960320@hotmail.com>
2019-01-23 14:07:47 +0800

    auth branch

--- src/main/java/info/maoyu/server/configuration/Auth.java
+++ src/main/java/info/maoyu/server/configuration/Auth.java
@@ -1,5 +1,7 @@
 package info.maoyu.server.configuration;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
