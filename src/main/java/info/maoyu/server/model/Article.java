9ecd5222d15f438107f4dd32c71be9d70840ca05
Yu Mao <maoyu960320@hotmail.com>
2019-02-03 11:43:38 +0800

    add validation, add setters

--- src/main/java/info/maoyu/server/model/Article.java
+++ src/main/java/info/maoyu/server/model/Article.java
@@ -1,34 +1,61 @@
 package info.maoyu.server.model;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 public class Article {
     private int id;
+
+    @NotEmpty(message = "Title is required")
     private String title;
+
     private Date created_at;
     private Date modified_at;
     private String author;
+
+    @NotEmpty(message = "Content is required")
     private String content;
     private String category;
     private int comments;
+    private String suffixCode;
 
-    public int getId(){
+    public int getId() {
         return this.id;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return this.title;
     }
 
-    public String getAuthor(){
+    public String getAuthor() {
         return this.author;
     }
 
-    public String getContent(){
+    public String getContent() {
         return this.content;
     }
 
-    public String getCategory(){
+    public String getCategory() {
         return this.category;
+    }
+
+    public String getSuffixCode() {
+        return this.suffixCode;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
